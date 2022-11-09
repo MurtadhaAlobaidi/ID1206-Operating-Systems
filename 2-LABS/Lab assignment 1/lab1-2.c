59281de871c3c1f89c81a5d6c8675341691cd3cd
Murtadha Alobaidi <mhaao@kth.se>
2022-11-09 23:33:25 +0100

    2-lab

--- 2-LABS/Lab assignment 1/lab1-2.c
+++ 2-LABS/Lab assignment 1/lab1-2.c
@@ -1,29 +1,28 @@
 #include <dirent.h> //files
 #include <errno.h>  /* error handling */
 #include <fcntl.h>  // mq_open
+//#include <mqueue.h> /* Queue functions */
 #include <stdio.h>  //input and output
 #include <stdlib.h> // library
 #include <string.h> //Size_t  and strlen
 #include <sys/stat.h>
 
+int read() {
+  FILE *file;
+  file = fopen("file.txt", "r");
+  char r;
+  while (!feof(file)) {
+    r = fgetc(file);
+    printf("%c", r);
+  }
+  fclose(file);
+
+  return r;
+}
+
+// int write() {}
+
 int main(int argc, char const *argv[]) {
-  FILE *ptr;
-  char ch;
-
-  // Opening file in reading mode
-  ptr = fopen("file.txt", "r");
-
-  // Printing what is written in file
-  // character by character using loop.
-  do {
-    ch = fgetc(ptr);
-    printf("%c", ch);
-
-    // Checking if character is not EOF.
-    // If it is EOF stop eading.
-  } while (ch != EOF);
-
-  // Closing the file
-  fclose(ptr);
+  read();
   return 0;
 }
