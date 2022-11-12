29d56ea197bfc301c9383e8de165844dba40880e
Murtadha Alobaidi <mhaao@kth.se>
2022-11-12 01:49:52 +0100

    ok

--- 2-LABS/Lab assignment 1/LAB2.c
+++ 2-LABS/Lab assignment 1/LAB2.c
@@ -5,58 +5,82 @@
 #include <stdio.h>  /* Basic input/output stream */
 #include <stdlib.h> /* Standard library */
 #include <string.h> /* Size_t, strlen */
-#include <sys/_types/_size_t.h>
+#define MAX_SIZE 100
+#define MAX_NUM_MSG 10 
 
-int MAX_SIZE = 100; 
-int MAX_NUM_MSG = 10; 
-char buf[MAX_SIZE]; 
-char *buf = calloc (sizeof * buf);
-char buf_rec[MAX_SIZE];
-char *buf_rec = calloc (sizeof * buf_rec);
-
+/*
 int read() {
+ 
+  return r;
+}*/
+
+int mq_sender(struct mq_attr attr, mqd_t mqd, char *my_mq){
+  
   FILE *file;
-  file = fopen("text.txt", "r");
+  file = fopen("file.txt", "r");
+  //char buf[MAX_SIZE]; 
+  char *buf = malloc (sizeof *buf*MAX_SIZE);
   size_t s=0;
+    int r;
+
+  
   if (!file)
        {
     perror("Error file not open ");
     exit(1);
   }
-  char r;
-  while (r = fgetc(file) != EOF) {
+  while ((r = fgetc(file)) != EOF) {
     buf[s++]= r;
     //printf("%c", r);
   }
   fclose(file);
-  return r;
-}
 
-int mq_send(struct mq_attr attr, mqd_t mqd,const char *my_mq){
 // Open an existing message queue
-mqd = mq_open(my_mq, O_WRONLY,&attr);
+mqd = mq_open(my_mq, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR,&attr);
 
 // Write from "file" to the message queue
 mq_send(mqd, buf, strlen(buf), 1);
 
 // Close the message queue
 mq_close(mqd);
+free(buf); /* free allocated memory */
 
 return 0;
 }
 
-int mq_receive(struct mq_attr attr, mqd_t mqd,const char *my_mq){
+int mq_receiveer(struct mq_attr attr, mqd_t mqd,char *my_mq)
+{
+  
+//char buf_rec[MAX_SIZE];
+char *buf_rec = malloc (sizeof * buf_rec*MAX_SIZE);
+
 // Create message queue
-mqd = mq_open(my_mq, O_RDONLY | O_CREAT, &attr);
+mqd = mq_open(my_mq, O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR, &attr);
 
 // Read the message from the message queue
-mq_receive(mqd, buf_rec, MAX_NUM_MSG, NULL); 
-printf("Message: %s\n", buf);
+mq_receive(mqd, buf_rec, MAX_SIZE, NULL); 
+//printf("Message: %s\n", buf);
 
 // Close the message queue
 mq_close(mqd);
 //removes the message queue identified by name.
 mq_unlink(my_mq);
+
+ int x=0;
+    for(int i = 0;buf_rec[i]; i++)
+    {
+        
+        if (buf_rec[i] == 50) {
+            x++;
+        }
+        if (i >0) {
+        x++;
+        }
+    }
+    printf("Your message is: %s\n", buf_rec);
+    printf("The wordes you have in the file is= %d\n", x );
+    printf("\n");
+
 }
 
 
@@ -80,29 +104,16 @@ int main(int argc, char const *argv[])
     attr.mq_msgsize = MAX_SIZE; // Maximum message size (in bytes)
     attr.mq_curmsgs = 0; // Number of messages currently in queue
     
-    read();
+    //read();
 
-    mq_send(attr,mqd,my_mq);
-    mq_receive(attr,mqd,my_mq);
+    mq_sender(attr,mqd,my_mq);
+    mq_receiveer(attr,mqd,my_mq);
 
     /* code */
-    print();
-    return 0;
+    //print();
 }
 
-int print(){
-  int x;
-    for(int i = 0;buf_rec[i]; i++)
-    {
-        
-        if (buf_rec[i] == 20) {
-            x++;
-        }
-        else if (i >0) {
-        x++;
-        }
-    }
-    printf("Your message is: %s\n", buf_rec);
-    printf("The wordes you have in the file is%d\n" , "[", x ,"]" );
+/*int print(){
+ 
     return 0;
-}
+}*/
