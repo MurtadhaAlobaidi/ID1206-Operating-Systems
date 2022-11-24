770091fb67d171fa5c1d552c0fff9544b274cf0f
Murtadha Alobaidi <mhaao@kth.se>
2022-11-24 17:17:23 +0100

    ok

---
+++ 2-LABS/Lab assignment 2/os_/lab224.c
@@ -0,0 +1,53 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*
+ * From Lecture_5
+ * */
+
+int buffer = 0;
+pthread_mutex_t lock;
+
+void *thread_func(void *arg) {
+
+  int *buf = (int *)malloc(sizeof(int));
+  *buf = 0;
+
+  while (buffer < 13) {
+    pthread_mutex_lock(&lock);
+    printf("TID: %d\t PID: %d\t Buffer: %d\n", (int)(intptr_t)pthread_self(),
+           getpid(), buffer);
+    (*buf)++;
+    buffer++;
+    pthread_mutex_unlock(&lock);
+  }
+
+  return buf;
+}
+
+int main(void) {
+
+  pthread_t threads123[3];
+  pthread_mutex_init(&lock, NULL);
+
+  //int i = 0;
+  pthread_create(&threads123[0], NULL, thread_func, NULL);
+  pthread_create(&threads123[1], NULL, thread_func, NULL);
+  pthread_create(&threads123[2], NULL, thread_func, NULL);
+  // while (i < 3) {
+  //   pthread_create(&threads123[i++], NULL, thread_func, NULL);
+  // }
+  /* inspiration from
+   * https://stackoverflow.com/questions/21396995/pthread-joinwarning-cast-from-pointer-to-integer-of-different-size
+   * */
+  for (int i = 0, *mod; i < 3; printf("TID %d worked on the buffer %d times\n",
+                                      (int)(intptr_t)threads123[i++], *mod)) {
+    pthread_join(threads123[i], (void **)&mod);
+  }
+
+
+printf("Total buffer accesses: %d \n\n", buffer);
+
+  return 0;
+}
